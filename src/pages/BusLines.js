798b6fe821df79b6e04002b65740d6d4273a1ca0
Singuila01 <mbayechris201@gmail.com>
2025-05-18 15:07:42 +0200

    Modif lignes de bus (indentation)

--- src/pages/BusLines.js
+++ src/pages/BusLines.js
@@ -5,36 +5,36 @@ function BusLines() {
   const [lines, setLines] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch('https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-topologie-lignes-td/records?order_by=id&limit=100')
-      .then(response => response.json())
-      .then(data => {
-        console.log('API Response:', data); // Utile pour voir la structure
-        setLines(data.results); // Accès direct à `results`
-        console.log(data.results); // Affiche les résultats dans la console
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error('Erreur lors de la récupération des données :', error);
-        setLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+        fetch('https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-topologie-lignes-td/records?order_by=id&limit=100')
+        .then(response => response.json())
+        .then(data => {
+            console.log('API Response:', data); // Utile pour voir la structure
+            setLines(data.results); // Accès direct à `results`
+            console.log(data.results); // Affiche les résultats dans la console
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données :', error);
+            setLoading(false);
+        });
+    }, []);
 
-  if (loading) return <p>Chargement...</p>;
+    if (loading) return <p>Chargement...</p>;
 
-  return (
-    <div className='bus-lines'>
-      <h1>Lignes de bus</h1>
-      <ul>
-        {lines.map((line, index) => (
-          <li key={index} style={{ backgroundColor: line.couleurligne }}>
-            <h1>{line.nomcourt}</h1>
-            <p className='nom_long'>({line.nomlong})</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className='bus-lines'>
+            <h1>Lignes de bus</h1>
+            <ul>
+                {lines.map((line, index) => (
+                <li key={index} style={{ backgroundColor: line.couleurligne }}>
+                    <h2>{line.nomcourt}</h2>
+                    <p className='nom_long'>({line.nomlong})</p>
+                </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default BusLines;
