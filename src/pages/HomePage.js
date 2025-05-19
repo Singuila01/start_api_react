9aee223339a01dfddd52e2069b08aff3fae215e2
Singuila01 <mbayechris201@gmail.com>
2025-05-19 08:44:12 +0200

    Modif de la page d'accueil

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const HomePage = () => (
     <div className='container'>
-        <div className='presentation'>
+        <div className='header'>
             <h1>Bienvenue sur Rennes Transport Explorer</h1>
+        </div>
+        <div className='presentation'>
+            <h1>Présentation</h1>
             <p>
                 Ce site vous permet d'explorer en détail toutes les informations du réseau de transport de Rennes grâce à l'API STAR. 
                 Retrouvez les horaires en temps réel, les itinéraires, les arrêts, les lignes de bus et de métro, ainsi que de nombreuses données utiles pour vos déplacements quotidiens.
@@ -32,6 +35,7 @@ const HomePage = () => (
             </ul>
         </div>
         <div className='description'>
+            <h1>Description</h1>
             <p>
                 Ce projet utilise l’API officielle STAR pour vous offrir une expérience complète et à jour sur les transports en commun de Rennes Métropole.
             </p>
