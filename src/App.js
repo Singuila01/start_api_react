9a252f448ba352c395b0f81c24b5199b2d32f7c3
Singuila01 <mbayechris201@gmail.com>
2025-05-19 21:06:21 +0200

    Ajout d'une nouvelle route pour les parc relais

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import BusLines from './pages/BusLines';
 import HomePage from './pages/HomePage';
 import MetroTime from './pages/MetroTime';
+import ParcRelais from './pages/ParcRelais';
 import Header from './Header';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/bus" element={<BusLines />} />
                 <Route path="/metro" element={<MetroTime />} />
+                <Route path="/parc" element={<ParcRelais />} />
             </Routes>
         </Router>
     );
